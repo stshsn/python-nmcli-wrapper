1c93cbf64214c6c8181d77aef6f33d409487f468
Satoshi HOSHINO <s-hoshino@ib-sol.co.jp>
2019-03-22 11:16:52 +0900

    パッケージ化用ファイルの修正
    * setuptools のインポートし忘れを修正
    * setup.py を修正

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+from setuptools import setup, find_packages
+
+readme = '''
+Python wrapper for NetworkManager-CLI(nmcli)
+
+To manipulate network configuration from python code on Linux, 
+you can use this module as a wrapper to nmcli.
+'''
+
 setup(
     name='python_nmcli_wrapper',
     version='0.1.0',
-    description='Simple wrapper for Network Manager CLI',
+    description='Python wrapper for NetworkManager-CLI(nmcli)',
     long_description=readme,
     author='Satoshi HOSHINO',
     author_email='s-hoshino@ib-sol.co.jp',
-    install_requires=['os', 'shlex', 'subprocess', 'enum']
+    install_requires=['os', 'shlex', 'subprocess', 'enum'],
     url='',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-
+    packages=find_packages(exclude=('tests', 'docs')),
+)
